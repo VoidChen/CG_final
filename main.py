a13e7d11c17275014a291cc99642616ed315c75e
VoidChen <void.tw@gmail.com>
2016-12-12 18:26:11 +0800

    Support image name argument

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import sys
 import itertools
 from PIL import Image
 from palette import *
 from util import *
 
 if __name__ == '__main__':
-    image = Image.open('input.jpg')
-    print(image.format, image.size, image.mode)
+    if len(sys.argv) > 1:
+        image_name = sys.argv[1]
+    else:
+        image_name = 'input.jpg'
+
+    image = Image.open(image_name)
+    print(image_name, image.format, image.size, image.mode)
 
     lab = rgb2lab(image)
 
