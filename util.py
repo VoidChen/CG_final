aa80075eb01226bb66afdb6f07d725b418927127
VoidChen <void.tw@gmail.com>
2016-12-12 17:51:42 +0800

    Add h_merge and v_merge

--- util.py
+++ util.py
@@ -12,3 +12,27 @@ def lab2rgb(image):
 
 def compare(image_a, image_b):
     print('compare', list(image_a.getdata()) == list(image_b.getdata()))
+
+def h_merge(images):
+    width = sum([image.width for image in images])
+    height = max([image.height for image in images])
+
+    merge = Image.new(images[0].mode, (width, height))
+    offset = 0
+    for image in images:
+        merge.paste(image, (offset, 0))
+        offset += image.width
+
+    return merge
+
+def v_merge(images):
+    width = max([image.width for image in images])
+    height = sum([image.height for image in images])
+
+    merge = Image.new(images[0].mode, (width, height))
+    offset = 0
+    for image in images:
+        merge.paste(image, (0, offset))
+        offset += image.height
+
+    return merge
