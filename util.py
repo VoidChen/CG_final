f3e937356f5fe8ab6fe568a877bfcd9e41154af2
VoidChen <void.tw@gmail.com>
2016-12-24 22:05:42 +0800

    Add LABtoRGB and ValidRGB

--- util.py
+++ util.py
@@ -10,6 +10,34 @@ def lab2rgb(image):
     LAB_p = ImageCms.createProfile('LAB')
     return ImageCms.profileToProfile(image, LAB_p, RGB_p, outputMode='RGB')
 
+def LABtoXYZ(LAB):
+    def f(n):
+        return n**3 if n > 6/29 else 3 * ((6/29)**2) * (n - 4/29)
+
+    L, a, b = LAB
+    assert(0 <= L <= 100 and -128 <= a <= 127 and -128 <= b <= 127)
+
+    X = 95.047 * f((L+16)/116 + a/500)
+    Y = 100.000 * f((L+16)/116)
+    Z = 108.883 * f((L+16)/116  - b/200)
+    return (X, Y, Z)
+
+def XYZtoRGB(XYZ):
+    def f(n):
+        return n*12.92 if n <= 0.0031308 else (n**(1/2.4)) * 1.055 - 0.055
+
+    X, Y, Z = [x/100 for x in XYZ]
+    R = f(3.2406*X + -1.5372*Y + -0.4986*Z) * 255
+    G = f(-0.9689*X + 1.8758*Y + 0.0415*Z) * 255
+    B = f(0.0557*X + -0.2040*Y + 1.0570*Z) * 255
+    return (R, G, B)
+
+def LABtoRGB(LAB):
+    return XYZtosRGB(LABtoXYZ(LAB))
+
+def ValidRGB(RGB):
+    return False not in [0 <= x <= 255 for x in RGB]
+
 def compare(image_a, image_b):
     print('compare', list(image_a.getdata()) == list(image_b.getdata()))
 
