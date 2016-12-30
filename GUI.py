c0df1ce4ae8a70d4b0b7dffd0f99806607173e4e
VoidChen <void.tw@gmail.com>
2016-12-30 17:37:08 +0800

    Minor fix

--- GUI.py
+++ GUI.py
@@ -36,17 +36,6 @@ class PaletteLabel(ImageLabel):
             #set palette
             self.setColor(LAB)
 
-def limit_scale(image, width, height):
-    if image.width > width or image.height > height:
-        if image.width/image.height > width/height:
-            scale_size = (width, width * image.height//image.width)
-        else:
-            scale_size = (height * image.width//image.height, height)
-
-        return image.resize(scale_size)
-    else:
-        return image
-
 def load_image(label_image, labels_palette):
     #get image
     image_name = QFileDialog.getOpenFileName()[0]
--- util.py
+++ util.py
@@ -125,3 +125,14 @@ def v_merge(images):
         offset += image.height
 
     return merge
+
+def limit_scale(image, width, height):
+    if image.width > width or image.height > height:
+        if image.width/image.height > width/height:
+            scale_size = (width, width * image.height//image.width)
+        else:
+            scale_size = (height * image.width//image.height, height)
+
+        return image.resize(scale_size)
+    else:
+        return image
